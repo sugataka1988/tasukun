9bfbdfd632d3c6c27489f2603f29e4fac2a8a1b9
sugataka1988 <rohikatagasu@gmail.com>
2020-06-11 14:47:27 +0900

    deliveriesモデルにレコード追加と各モデルにバリデーション追加

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,2 +1,6 @@
 class Delivery < ApplicationRecord
+  validates :shipping_method, {presence: true}
+  validates :delivery_company_name, {presence: true}
+  validates :delivery_carrier_phone_number, {presence: true}
+  validates :delivery_company_address, {presence: true}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :user_name, {presence: true}
+    validates :mail_address, {presence: true}
+    validates :password, {presence: true, length: {minimum: 6 ,maximum: 12}}
+    
 end
