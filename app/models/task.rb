277e8e0597f05fb042f059573413901f29a1513e
sugataka1988 <rohikatagasu@gmail.com>
2020-06-25 14:56:08 +0900

    ステップ6-2　PR修正分　不要なかっこをはずす

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
-    validates :user_id, {presence: true}
-    validates :reception_date, {presence: true}
-    validates :task_content, {presence: true}
-    validates :number_of_requests, {presence: true}
-    validates :desired_completion_date, {presence: true}
+    validates :user_id, presence: true
+    validates :reception_date, presence: true
+    validates :task_content, presence: true
+    validates :number_of_requests, presence: true
+    validates :desired_completion_date, presence: true
 end
