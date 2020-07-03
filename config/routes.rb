4d25388696286fbbeedcef8d89530527592b128e
sugataka1988 <rohikatagasu@gmail.com>
2020-07-03 15:40:43 +0900

    ステップ7-1: タスクの一覧画面、詳細画面を作成しましょう 完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   root to: redirect('/tasks')
   get 'home/top'
   get 'tasks/index'
   get 'users/index'
   get 'deliveries/index'
-=======
-  resources :task_contents
->>>>>>> origin
+  get 'task_contents/index'
   resources :deliveries
   resources :tasks
   resources :users
+  resources :task_contents
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
