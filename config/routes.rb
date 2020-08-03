820db0fce09c63b0e60b285b4d4b27e4b464cbdf
sugataka1988 <rohikatagasu@gmail.com>
2020-08-03 14:08:18 +0900

    ステップ7-1　追加課題  指摘事項の修正2回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-#  get "labels" => "labels#index"
-#  get "labels/new" => "labels#new"
-#  get "labels/:id" => "labels#show"
-#  get "labels/:id/edit" => "labels#edit"
-  
-#  post "labels/create" => "labels#create"
-#  post "labels/:id/update" => "labels#update", as: :lable_update
-#  post "labels/:id/destroy" => "labels#destroy"
-  
-  root to: redirect('/tasks')
-  # TODO: atode yaru
   resources :labels
   resources :deliveries
   resources :tasks
