f282ce428756691f087a5d44a7f8f4b9b23c30de
sugataka1988 <rohikatagasu@gmail.com>
2020-08-04 14:01:05 +0900

    ステップ7-1　追加課題  指摘事項の修正3回目

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :task_contents
   resources :labels
   resources :deliveries
   resources :tasks
   resources :users
-  resources :task_contents
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
