f0c6e4bf9deb6078662f2798a274e2f7f55c565d
sugataka1988 <rohikatagasu@gmail.com>
2020-08-06 13:16:46 +0900

    ステップ9　factory_botの導入

--- spec/models/task_content_spec.rb
+++ spec/models/task_content_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 RSpec.describe TaskContent, type: :model do
   it "task_contentとdesired_completion_dateが入力されている場合、有効である" do
-    task_content = TaskContent.new(task_content: "naiyou", desired_completion_date: "2017/1/1 10:00")
+    task_content = build(:task_content)
     # expect(task_content).to be_valid
     expect(task_content.valid?).to eq true
   end
+  
   it "task_contentがない場合、無効である" do
-    task_content = TaskContent.new(task_content: "", desired_completion_date: "2017/1/1 10:00")
+    task_content = build(:task_content, task_content: nil)
     expect(task_content.valid?).to eq false
   end
     
   it "desired_completion_dateがない場合、無効である" do
-   task_content = TaskContent.new(task_content: "naiyou")
+   task_content = build(:task_content, desired_completion_date: nil)
    expect(task_content.valid?).to eq false
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,25 +4,29 @@ RSpec.describe User, type: :model do
     user = User.new(user_name: "tanaka", mail_address: "yahoo", password: "password")
     expect(user.valid?).to eq true
   end
+  
   it "user_nameがない場合、無効" do
     user = User.new(user_name: "", mail_address: "yahoo", password: "password")
     expect(user.valid?).to eq false
   end
+  
   it "mail_addressがない場合、無効" do
     user = User.new(user_name: "tanaka", password: "password")
     expect(user.valid?).to eq false
   end
+  
   it "passwordがない場合、無効" do
     user = User.new(user_name: "tanaka", mail_address: "yahoo", password: "")
     expect(user.valid?).to eq false
   end
+  
   it "passwordの字数が5字以下の場合、無効" do
     user = User.new(user_name: "tanaka", mail_address: "yahoo", password: "pas")
     expect(user.valid?).to eq false
   end
+  
   it "passwordの字数が13字以上の場合、無効" do
     user = User.new(user_name: "tanaka", mail_address: "yahoo", password: "passwordpassword")
     expect(user.valid?).to eq false
   end
-  
 end
